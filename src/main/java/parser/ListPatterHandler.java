2cba6eba4eca3bf7ed4d08be4b9d6b62f29735ff
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2021-04-03 15:59:07 +0100

    list patternmatcher

--- src/main/java/parser/ListPatterHandler.java
+++ src/main/java/parser/ListPatterHandler.java
@@ -1,9 +1,12 @@
 package parser;
 
+import java.util.regex.Pattern;
+
 public class ListPatterHandler implements PatternHandler {
+    Pattern pattern = Pattern.compile("\\d+,\\d+");
     @Override
     public boolean canHandle(String value) {
-        return false;
+        return pattern.matcher(value).matches();
     }
 
     @Override
--- src/test/java/parser/ListPatterHandlerTest.java
+++ src/test/java/parser/ListPatterHandlerTest.java
@@ -1,7 +1,28 @@
 package parser;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 class ListPatterHandlerTest {
+    private PatternHandler handler;
 
+    @BeforeEach
+    void setUp() {
+        handler = new ListPatterHandler();
+    }
+
+    @Test
+    void returnsTrueWhenThePatternMatches() {
+        String expressionPart = "1,2";
+        assertThat(handler.canHandle(expressionPart), is(true));
+    }
+
+    @Test
+    void returnsFalseWhenThePatterDoesNotMatches() {
+        String expressionPart = "1";
+        assertThat(handler.canHandle(expressionPart), is(false));
+    }
 }
